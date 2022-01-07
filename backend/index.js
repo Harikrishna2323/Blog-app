970b4a80099d0d208840f46e8a251524c888a652
harikrishna2323 <harikb23@gmail.com>
2022-01-07 17:57:13 +0530

    changes in index.js

--- backend/index.js
+++ backend/index.js
@@ -48,10 +48,10 @@ app.use("/api/categories", categoryRouter);
 app.use("/images", express.static(path.resolve(__dirname, "/images")));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/frontend/build"));
+  app.use(express.static("frontend/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/frontend/build/index.html"));
+    res.sendFile(path.join(__dirname, "frontend/build/index.html"));
   });
 }
 
